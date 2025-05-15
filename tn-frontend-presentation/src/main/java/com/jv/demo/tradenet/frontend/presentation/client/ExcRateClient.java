369e14170fc9a129ca0d83e08163bf1208b3c084
jeyavignesh7 <jeyavignesh@gmail.com>
2025-05-15 11:03:34 -0400

    Implemented Docker Compose

--- tn-frontend-presentation/src/main/java/com/jv/demo/tradenet/frontend/presentation/client/ExcRateClient.java
+++ tn-frontend-presentation/src/main/java/com/jv/demo/tradenet/frontend/presentation/client/ExcRateClient.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-//@FeignClient(value="excrate-client", url="http://localhost:2222/frontend-service")
-@FeignClient("${tn.frontend.service.excrate.url}")
+//@FeignClient(value="excrate-client", url="http://tn-frontend-service-inst1:2222/frontend-service")
+//@FeignClient(name="${tn.frontend.service.excrate.host}", path="${tn.frontend.service.excrate.path}")
+@FeignClient(name="FRONTEND-SERVICE", path="/frontend-service")
 @LoadBalancerClient
 public interface ExcRateClient {
 
